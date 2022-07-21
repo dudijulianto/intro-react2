44b2ade48c1fc32301554e82954ce03f93727181
dudijulianto <dudi.julianto23@gmail.com>
2022-07-21 21:26:55 +0700

    cek

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,22 @@
+import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Card from './components/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavigationBar from './components/NavigationBar';
+import { Provider } from 'react-redux';
 
+import store from './store';
+import Card from './components/card';
+import Detail from './pages/detail';
+import Home from './pages/home';
+import Router from './routes';
 
 function App() {
 
-  const [state, setState] = useState([]);
-  const[secondState, setSecondState] = useState([]);
-
-  useEffect(() =>{
-    axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=c99a687890015fbe80f81d279426568d&language=en-US&page=1').then(resp =>{
-      if (resp.data){
-        setState(resp.data.results);
-      }
-    })
-      .catch(err =>{
-        console.log(err.message, '<<<<<error');
-      });
-    }, []);
-
-
-  return (
-    <div className="App">
-      <div className="myBg">
-        <NavigationBar />
-      </div>
-      <div className='list'>
-        {state.map(item => (
-          <Card
-            movies={item}
-            />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<Provider store={ store }>
+			<Router />
+		</Provider>
+	);
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
+import './styles.css';
 
 
+const Card = ({ movie, key }) => {
 
-const Card = ({ movies }) => {
-
-	return (
-		<div className='card' >
-			<img src={ 'https://image.tmdb.org/t/p/w500' + movies.poster_path } height='100px' />
-			<h5>{ movies.title }</h5>
-			<p>{ movies.overview }</p>
+	if (movie) return (
+		<div key={ movie.id } className='Card' >
+			<img src={ 'https://image.tmdb.org/t/p/w500' + movie.poster_path } height='100px' />
+			<h5>{ movie.title }</h5>
+			<p>{ movie.overview }</p>
 		</div>
 	);
+
+	return null;
 };
 
 export default Card;
